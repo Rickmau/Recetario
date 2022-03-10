f495a67d188eada8a1838c070cc89bb84d764e8c
Mauricio Erick Soria Cisneros <100866034+Rickmau@users.noreply.github.com>
2022-03-10 11:38:05 -0600

    Clase ingrediente completa

--- src/Ingredients.java
+++ src/Ingredients.java
@@ -6,4 +6,38 @@ public class Ingredients {
         Double peso;
      */
 
+    private String name;
+    private boolean esSalado;
+    private double peso;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public boolean isEsSalado() {
+        return esSalado;
+    }
+
+    public void setEsSalado(boolean esSalado) {
+        this.esSalado = esSalado;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    public Ingredients(String name, boolean esSalado, double peso){
+        this.name = name;
+        this.esSalado = esSalado;
+        this.peso = peso;
+        System.out.println("Ingrediente creado");
+    }
 }
